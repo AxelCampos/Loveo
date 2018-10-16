29fe121476b641bc6e0e700b1ff00d65a6e3f1c6
Axel Campos <axelcamposbonnet@gmail.com>
2018-10-16 10:21:56 +0200

    Changes on schema and add sqlite and sequelize

--- server/data/schema.js
+++ server/data/schema.js
@@ -8,27 +8,63 @@ export const typeDefs = gql`
     id: Int! # unique id for the group
     name: String # name of the group
     users: [User!]! # users in the group
-    messages: [Message!]! # messages sent to the group
+    GroupMessage: [GroupMessage!]! # messages sent to the group
+    album: [Photo!]!
   }
   # a user -- keep type really simple for now
   type User {
     id: Int! # unique id for the user
     email: String! # we will also require a unique email per user
-    username: String # this is the name we'll show other users
-    messages: [Message!]! # messages sent by user
-    messagestwo: [Messagetwo!]!
+    username: String! # this is the name we'll show other users
+    UserMessage: [UserMessage!]! # messages sent by user
+    GroupMessage: [GroupMessage!]!
     groups: [Group!]! # groups the user belongs to
     friends: [User] # user's friends/contacts
+    album: [Photo!]!
+  }
+
+  union to = User | Group
+
+  type Photo {
+    id: Int!
+    name: String!
+    createdAt: Date!
+    from: User!
+    to: to
+  }
+
+  #type PhotoUser implements Photo {
+  # id: Int!
+  #name: String!
+  #createdAt: Date!
+  #from: User!
+  #to: User | Group
+
+  #}
+
+  #type PhotoGroup implements Photo {
+  #  id: Int!
+  # name: String!
+  # createdAt: Date!
+  # from: User!
+  # to: Group!
+  # }
+
+  interface Message {
+    id: Int! # unique id for message
+    from: User! # user who sent the message
+    text: String! # message text
+    createdAt: Date! # when message was created
   }
   # a message sent from a user to a group
-  type Message {
+  type GroupMessage implements Message {
     id: Int! # unique id for message
     to: Group! # group message was sent in
     from: User! # user who sent the message
     text: String! # message text
     createdAt: Date! # when message was created
   }
-  type Messagetwo {
+  type UserMessage implements Message {
     id: Int! # unique id for message
     to: User! # group message was sent in
     from: User! # user who sent the message
@@ -41,8 +77,12 @@ export const typeDefs = gql`
     user(email: String, id: Int): User
     # Return messages sent by a user via userId
     # Return messages sent to a group via groupId
-    messagesg(groupId: Int): [Message]
-    messagesu(userId: Int): [Messagetwo]
+    groupmessages(groupId: Int): [GroupMessage]
+    usermessages(userId: Int): [UserMessage]
+    # Return photos sent by a user via id
+    # Return photos sent to a group via id
+    photo(userId: Int, groupId: Int): [Photo]
+    search: [to]
     # Return a group by its id
     group(id: Int!): Group
   }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import { ApolloServer } from 'apollo-server';
+import { resolvers } from './data/resolvers';
 import { typeDefs } from './data/schema';
-import { mocks } from './data/mocks';
+import mockDB from './data/mocks';
 
 const PORT = 8080;
 
-const server = new ApolloServer({ typeDefs, mocks });
-server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
+const startServer = async () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await server.listen({ port: PORT });
+  console.log(`🚀 Server ready at ${url}`);
+};
+
+const init = async () => {
+  await mockDB({ populating: true, force: true });
+  startServer();
+};
+
+init();
