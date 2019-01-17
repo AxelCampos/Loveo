278fec3c3136ebe24c099b8504b4022bc4f271e1
Axel Campos <axelcamposbonnet@gmail.com>
2019-01-17 18:15:03 +0100

    implement Image Picker

--- client/android/app/src/main/java/com/chatty/MainApplication.java
+++ client/android/app/src/main/java/com/chatty/MainApplication.java
@@ -3,6 +3,7 @@ package com.chatty;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
             new ReactNativeConfigPackage()
