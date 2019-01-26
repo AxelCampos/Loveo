870c20b2fffc59e289565230714ce05271f2c53f
rostorres <roselle.dnlc@gmail.com>
2019-01-26 01:17:38 +0100

    added Marks to each user in near.screen map, for now directions faked

--- client/src/screens/near.screen.js
+++ client/src/screens/near.screen.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { graphql, compose } from 'react-apollo';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { USERS_QUERY } from '../graphql/users.query';
-import { USER_QUERY } from '../graphql/user.query';
 import withLoading from '../components/withLoading';
 
 const styles = StyleSheet.create({
@@ -14,11 +13,40 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
-    margin: 10,
-    height: 300,
-    width: 300,
+    height: 550,
+    width: 400,
   }
 });
+
+//TODO: convert real directions into coordinates with Geolocation
+
+class SingleMarker extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  goToProfile = (user) => () => {
+    const { properties: { navigation: { navigate } } } = this.props;
+    navigate('Profile', { userId: user.id });
+  };
+
+  render() {
+    const { user } = this.props
+    const aleatLat = Math.random() / 50;
+    const aleatLong = Math.random() / 50;
+    return (
+      <Marker
+        coordinate={{
+          latitude: 40.416700 + aleatLat,
+          longitude: -3.703700 + aleatLong,
+        }}
+        title={user.username}
+        //description={"description"}
+        onPress={this.goToProfile(user)}
+      />
+    );
+  }
+}
 
 class Nearer extends Component {
   constructor(props) {
@@ -30,11 +58,18 @@ class Nearer extends Component {
         longitude: -3.703790,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
-      }
+      },
     };
   }
 
+  onPress = () => {
+    const { users } = this.props;
+    //console.log(this.state.markers);
+    return users.filter(a => (a.id !== a.id));
+  };
+
   render() {
+    const { users } = this.props;
     return (
       <View style={styles.container}>
         <MapView
@@ -42,13 +77,26 @@ class Nearer extends Component {
           style={styles.map}
           initialRegion={this.state.region}
           zoomEnabled={true}
-        />
+          onPress={this.onPress}
+        >
+          {users.map((user, index) => (
+            <SingleMarker key={index} user={user} properties={this.props} />
+          ))
+          }
+        </MapView>
       </View>
     );
   }
 }
 
+const usersQuery = graphql(USERS_QUERY, {
+  options: () => ({}), // fake the user for now
+  props: ({ data: { users } }) => ({
+    users: users || [],
+  }),
+});
+
 export default compose(
-  //usersQuery,
+  usersQuery,
   withLoading,
 )(Nearer);
